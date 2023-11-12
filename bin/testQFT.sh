b4532ba803ea6f5fa5a6d4f7a095e8d4ba6a1f7d
Gilberto Javier Díaz Toro <gilberto.diaz@gmail.com>
2023-11-12 06:08:18 -0500

    testQFT.sh minor fix

--- bin/testQFT.sh
+++ bin/testQFT.sh
@@ -2,8 +2,8 @@
 
 for numQubits in `seq 1 15`
 do
-	echo $numQubits '##########################################' >> TMFQSfullState_QFT_results
-	(time ./qft $numQubits 0) &>> TMFQS_QFT_results
+	echo $numQubits '##########################################' >> TMFQS_QFT_results
+	(time ./qft $numQubits 0) &>> TMFQSfullState_QFT_results
 done
 
 
